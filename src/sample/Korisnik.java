c7da244f806543425b89e133a5825632cb0cb223
student <hsmajic1@etf.unsa.ba>
2019-04-08 12:53:34 +0200

    Initial commit

---
+++ src/sample/Korisnik.java
@@ -0,0 +1,79 @@
+package sample;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Korisnik {
+    private SimpleStringProperty ime;
+    private SimpleStringProperty prezime;
+    private SimpleStringProperty email;
+    private SimpleStringProperty korisnickoIme;
+    private SimpleStringProperty lozinka;
+
+    public Korisnik{
+        ime = new SimpleStringProperty();
+        prezime = new SimpleStringProperty();
+        email = new SimpleStringProperty();
+        korisnickoIme = new SimpleStringProperty();
+        lozinka = new SimpleStringProperty();
+    }
+
+    public String getIme() {
+        return ime.get();
+    }
+
+    public SimpleStringProperty imeProperty() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime.set(ime);
+    }
+
+    public String getPrezime() {
+        return prezime.get();
+    }
+
+    public SimpleStringProperty prezimeProperty() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime.set(prezime);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public SimpleStringProperty emailProperty() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public String getKorisnickoIme() {
+        return korisnickoIme.get();
+    }
+
+    public SimpleStringProperty korisnickoImeProperty() {
+        return korisnickoIme;
+    }
+
+    public void setKorisnickoIme(String korisnickoIme) {
+        this.korisnickoIme.set(korisnickoIme);
+    }
+
+    public String getLozinka() {
+        return lozinka.get();
+    }
+
+    public SimpleStringProperty lozinkaProperty() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka.set(lozinka);
+    }
+}
